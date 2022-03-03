8b2fbf4f4add00be0d3b6a45ff55340daf64acba
MrAlexGert <alex.gert1812@.gmail.com>
2022-03-03 14:28:40 +0300

    add base styles pages and folder

--- next.config.js
+++ next.config.js
@@ -1,6 +1,35 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')
+const { createSecureHeaders } = require('next-secure-headers')
+const dev = process.env.NODE_ENV !== 'production'
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  poweredByHeader: false,
+  i18n: {
+    locales: ['ru', 'kz', 'uz'],
+    defaultLocale: 'ru',
+    localeDetection: false
+  },
+  images: {
+    domains: []
+  },
+  pwa: {
+    dest: 'public',
+    disable: dev,
+    register: true,
+    scope: '/'
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: createSecureHeaders({
+          referrerPolicy: 'no-referrer-when-downgrade',
+          frameGuard: false
+        })
+      }
+    ]
+  }
+})
---
+++ pages/_offline.tsx
@@ -0,0 +1,11 @@
+
+
+const PageOffline = () => {
+  return (
+    <>
+     <div>offline</div>
+    </>
+  )
+}
+
+export default PageOffline
