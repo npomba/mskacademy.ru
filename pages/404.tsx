16f658ba78f0bcdcb1ae5e2cfdbccbfe38ce516b
MrAlexGert <alex.gert1812@.gmail.com>
2022-03-09 13:43:55 +0300

    add_pages

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import stls from '@/styles/pages/Page404.module.sass'
+
+
+const Page404 = () => {
+  return (
+    <>
+      <div>404</div>
+    </>
+  )
+}
+
+export default Page404
---
+++ pages/500.tsx
@@ -0,0 +1,12 @@
+import stls from '@/styles/pages/Page500.module.sass'
+
+
+const Page500 = () => {
+  return (
+    <>
+     <div>500</div>
+    </>
+  )
+}
+
+export default Page500
