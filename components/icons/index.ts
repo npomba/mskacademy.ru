049adcf6897cfc6706f89ef5c0f6f2b8ebb217f0
MrAlexGert <alex.gert1812@.gmail.com>
2022-03-10 18:54:35 +0300

    added IconGeneralRABO

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -1,1 +1,2 @@
 export { default as IconGeneralLogo } from '@/components/icons/general/IconGeneralLogo'
+export { default as IconGeneralRABO } from '@/components/icons/general/IconGeneralRABO'
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import stls from '@/styles/pages/PageHome.module.sass'
 import type { NextPage } from 'next'
-import { IconGeneralLogo } from '@/components/icons'
+import { IconGeneralLogo, IconGeneralRABO } from '@/components/icons'
 import { GeneralContactCTA } from '@/components/general'
 import { phoneNumber, email } from '@/config/index'
 
@@ -11,6 +11,7 @@ const Home: NextPage = () => {
       <IconGeneralLogo />
       <GeneralContactCTA href={phoneNumber.href} value={phoneNumber.val} lable= {'test phone'} variant={'size-m'}/>
       <GeneralContactCTA href={email.href} value={email.val} lable= {'test email'} variant={'size-xl'}/>
+      <IconGeneralRABO/>
     </>
   )
 }
