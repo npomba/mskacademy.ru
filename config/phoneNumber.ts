a297c5b282dfa7ad31395dd6d1a01c8244f8b161
MrAlexGert <alex.gert1812@.gmail.com>
2022-03-10 14:23:55 +0300

    added phoneNumber

---
+++ config/phoneNumber.ts
@@ -0,0 +1,11 @@
+type TypePhoneNumber = {
+    href: `tel:+${string}`
+    val: `+${string}`
+  }
+  
+  const phoneNumber: TypePhoneNumber = {
+    href: 'tel:+7-495-648-62-26',
+    val: '+7 (495) 648-62-26'
+  }
+  
+  export default phoneNumber
