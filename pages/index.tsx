2c6db1134c39dbaf15bd2e512283c09933a908d4
Novailoveyou <novailoveyoux@gmail.com>
2022-03-14 15:05:23 +0300

    removed unused imports from index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,8 @@
 import stls from '@/styles/pages/PageHome.module.sass'
 import type { NextPage } from 'next'
-import { IconGeneralLogo } from '@/components/icons'
-import { GeneralContactCTA, GeneralLogo } from '@/components/general'
-import { phoneNumber, email } from '@/config/index'
-import { ImgGeneralGMDELogo } from '@/components/images'
-
-
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <IconGeneralLogo />
-      <GeneralContactCTA href={phoneNumber.href} value={phoneNumber.val} lable= {'test phone'} variant={'size-m'}/>
-      <GeneralContactCTA href={email.href} value={email.val} lable= {'test email'} variant={'size-xl'}/>
-      <ImgGeneralGMDELogo/>
-      <GeneralLogo/>
-    </>
-  )
+  return <></>
 }
 
 export default Home
