39371b9a060a1b576a97757619747a7fae5e2635
MrAlexGert <alex.gert1812@.gmail.com>
2022-03-16 13:23:57 +0300

    added HeaderMenuMobile

--- components/header/HeaderMenuMobile.tsx
+++ components/header/HeaderMenuMobile.tsx
@@ -1,16 +1,57 @@
 import stls from '@/styles/components/header/HeaderMenuMobile.module.sass'
+import Link from 'next/link'
 import { TypeGeneralClassNames } from '@/types/index'
+import { email, phoneNumber } from '@/config/index'
 import cn from 'classnames'
 import { getClassNames } from '@/helpers/index'
+import { Wrapper } from '@/components/layout'
+import { GeneralContactCTA, GeneralCopyright, GeneralLogo } from '@/components/general'
+import { dataGeneralMenuLinks } from '@/data/index'
+import { IconGeneralCross } from '@/components/icons'
+
+
 
 
 type TypeHeaderMenuMobileProps = TypeGeneralClassNames
 
 const HeaderMenuMobile = ({ classNames }: TypeHeaderMenuMobileProps) => {
-  
+
   return (
     <div className={cn(stls.container, getClassNames({ classNames })) || undefined}>
-     
+      <Wrapper classNames={[stls.wrapper]}>
+        <div className={stls.top}>
+          <GeneralLogo classNames={[stls.iconLogo]}/>
+          <IconGeneralCross classNames={[stls.iconCross]}/>
+        </div>
+        <ul className={stls.list}>
+          {
+            dataGeneralMenuLinks.map((item, idx) => (
+              <li className={stls.item} key={idx}>
+                <Link href={item.href}>
+                  <a className={stls.link}>{item.title}</a>
+                </Link>
+              </li>
+            ))
+          }
+        </ul>
+        <div className={stls.contscts}>
+          <GeneralContactCTA
+            classNames={[stls.contact]}
+            href={email.href}
+            value={email.val}
+            lable={'По вопросам сотрудничества'}
+            variant={'size-m'}
+          />
+          <GeneralContactCTA
+            classNames={[stls.contact]}
+            href={phoneNumber.href}
+            value={phoneNumber.val}
+            lable={'Бесплатно по России'}
+            variant={'size-m'}
+          />
+          <GeneralCopyright classNames={[stls.copyright]}/>
+        </div>
+      </Wrapper>
     </div>
   )
 }
