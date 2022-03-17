96b059f4dc3422476ca960be3915c5733e7c174e
MrAlexGert <alex.gert1812@.gmail.com>
2022-03-17 14:22:54 +0300

    fix

--- components/icons/general/IconGeneralCross.tsx
+++ components/icons/general/IconGeneralCross.tsx
@@ -14,15 +14,15 @@ const IconGeneralCross = ({ classNames }: TypeIconGeneralCrossProps) => {
       <svg viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M25 1L1 25"
           stroke={colors.psi}
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth='2'
+          strokeLinecap='round'
+          strokeLinejoin='round'
         />
         <path d="M1 1L25 25"
           stroke={colors.psi}
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth='2'
+          strokeLinecap='round'
+          strokeLinejoin='round'
         />
       </svg>
     </div>
