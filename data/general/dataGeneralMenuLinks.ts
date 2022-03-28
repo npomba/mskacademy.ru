e2e9e67c12a084a14d4946b9d7eebce64bf1ddbf
Novailoveyou <novailoveyoux@gmail.com>
2022-03-28 13:04:14 +0300

    fixed indentation in dataGeneralMenuLinks

--- data/general/dataGeneralMenuLinks.ts
+++ data/general/dataGeneralMenuLinks.ts
@@ -1,16 +1,16 @@
 const dataGeneralMenuLinks = [
-    {
-        title: 'Программы',
-        href: '#'
-    },
-    {
-        title: 'Об академии',
-        href: '#'
-    },
-    {
-        title: 'Дипломы',
-        href: '#'
-    }
+  {
+    title: 'Программы',
+    href: '#'
+  },
+  {
+    title: 'Об академии',
+    href: '#'
+  },
+  {
+    title: 'Дипломы',
+    href: '#'
+  }
 ]
 
 export default dataGeneralMenuLinks
