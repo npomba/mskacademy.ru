e94d8f63cc046aab32e0f84ec2b96d124f2c1d2d
Novailoveyou <novailoveyoux@gmail.com>
2022-03-30 22:33:17 +0300

    added gtm script & default seo config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,62 @@
 import '@/styles/app.sass'
 import type { AppProps } from 'next/app'
+import { TypeGeneralRoute } from '@/types/index'
+import { useEffect, useState } from 'react'
+import Router from 'next/router'
+import Script from 'next/script'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+import { DefaultSeo, LogoJsonLd } from 'next-seo'
+import SEO from 'seo.config'
+import { routesFront, selectors, gtm, prod } from '@/config/index'
+import { pageview } from '@/helpers/index'
 import { Header, Main, Footer } from '@/components/layout'
 import { ContextProgramsState, ContextProgramState } from '@/context/index'
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, pageProps, router }: AppProps) => {
+  const [loading, setLoading] = useState(false)
+
+  if (prod) {
+    console.log = () => {}
+  }
+
+  useEffect(() => {
+    // TagManager.initialize({ gtmId, dataLayerName: 'dataLayer' })
+
+    // handleUtms({ router })
+    // handleReferer()
+
+    NProgress.configure({
+      showSpinner: false
+    })
+
+    const start = () => {
+      NProgress.start()
+      setLoading(true)
+    }
+    const end = (url: TypeGeneralRoute) => {
+      NProgress.done()
+      setLoading(false)
+      pageview({ url })
+    }
+    Router.events.on('routeChangeStart', start)
+    Router.events.on('routeChangeComplete', end)
+    Router.events.on('routeChangeError', end)
+    return () => {
+      Router.events.off('routeChangeStart', start)
+      Router.events.off('routeChangeComplete', end)
+      Router.events.off('routeChangeError', end)
+    }
+  }, [router])
+
   return (
     <>
       {/* TODO: view header to make sure structure & styles are right */}
+      <DefaultSeo {...SEO} />
+      {/* <LogoJsonLd
+        logo={`${routesFront.root}${routesFront.assetsImgsIconsManifestIcon512}`}
+        url={routesFront.root}
+      /> */}
       <ContextProgramsState>
         <ContextProgramState>
           <Header />
@@ -16,6 +66,19 @@ const App = ({ Component, pageProps }: AppProps) => {
           <Footer />
         </ContextProgramState>
       </ContextProgramsState>
+      {prod && (
+        <Script
+          id={selectors.gtm}
+          dangerouslySetInnerHTML={{
+            __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${gtm}');`
+          }}
+        />
+      )}
     </>
   )
 }
