c10afa724659f1624de990d5e1258cc3b6528369
Novailoveyou <novailoveyoux@gmail.com>
2022-04-11 13:09:14 +0300

    updated number

--- config/phoneNumber.ts
+++ config/phoneNumber.ts
@@ -1,13 +1,13 @@
-import { TypeGeneralPhoneValue, TypeGeneralPhoneLink} from "@/types/index"
+import { TypeGeneralPhoneValue, TypeGeneralPhoneLink } from '@/types/index'
 
 type TypePhoneNumber = {
-    href: TypeGeneralPhoneLink
-    val: TypeGeneralPhoneValue
-  }
-  
-  const phoneNumber: TypePhoneNumber = {
-    href: 'tel:+7-495-648-62-26',
-    val: '+7 (495) 648-62-26'
-  }
-  
-  export default phoneNumber
+  href: TypeGeneralPhoneLink
+  val: TypeGeneralPhoneValue
+}
+
+const phoneNumber: TypePhoneNumber = {
+  href: 'tel:+7-495-149-00-20',
+  val: '+7 (495) 149-00-20'
+}
+
+export default phoneNumber
